40f61fd444113d48dc83dcc1563eced746cffed5
Shift <shift@laravelshift.com>
2024-06-08 05:53:49 +0000

    Re-register HTTP middleware

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -14,7 +14,16 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        //
+        $middleware->redirectGuestsTo(fn () => route('login'));
+        $middleware->redirectUsersTo(RouteServiceProvider::HOME);
+
+        $middleware->append(\Pollen\Http\Middleware\RequestStore::class);
+
+        $middleware->throttleApi();
+
+        $middleware->alias([
+            'theme' => \Qirolab\Theme\Middleware\ThemeMiddleware::class,
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
