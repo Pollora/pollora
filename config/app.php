db142348f7bb2fd571eea696ab4de745ba303d78
Shift <shift@laravelshift.com>
2024-06-08 05:53:49 +0000

    Re-register service providers

--- config/app.php
+++ config/app.php
@@ -7,21 +7,6 @@ use Illuminate\Support\ServiceProvider;
 
 return [
 
-    'providers' => ServiceProvider::defaultProviders()->merge([
-        /*
-         * Package Service Providers...
-         */
-
-        /*
-         * Application Service Providers...
-         */
-        App\Providers\AppServiceProvider::class,
-        App\Providers\AuthServiceProvider::class,
-        // App\Providers\BroadcastServiceProvider::class,
-        App\Providers\EventServiceProvider::class,
-        App\Providers\RouteServiceProvider::class,
-        App\Providers\MenuServiceProvider::class,
-    ])->toArray(),
 
     'conditions' => [
         'is_404' => '404',
