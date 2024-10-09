34bb50967148a98df5df754f2e973506e4cba98f
Olivier Gorzalka <olivier@amphibee.fr>
2024-10-09 09:06:06 +0200

    Reset default vite config

--- vite.config.js
+++ vite.config.js
@@ -1,32 +1,11 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
-import { readFileSync } from 'node:fs'
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: [
-                'resources/js/app.js'
-            ],
+            input: ['resources/css/app.css', 'resources/js/app.js'],
             refresh: true,
         }),
     ],
-    build: {
-      emptyOutDir: false,
-    },
-    server: {
-        cors: true,
-        strictPort: true,
-        port: 5173,
-        host: '0.0.0.0',
-        open: false,
-        hmr: {
-            port: 5173,
-            clientPort: 5173,
-        },
-        https: {
-            key: readFileSync('/etc/certs/local-key.pem'),
-            cert: readFileSync('/etc/certs/local-cert.pem'),
-        }
-    },
 });
