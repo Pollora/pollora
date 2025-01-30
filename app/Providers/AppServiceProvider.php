0cd917e5bb3d18cc4406d4d609c447be63f9354e
Olivier Gorzalka <olivier@amphibee.fr>
2025-01-30 07:57:20 +0100

    Fix some errors

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -21,15 +21,6 @@ class AppServiceProvider extends ServiceProvider
     public const HOME = '/home';
 
     /**
-     * The path to your application's "home" route.
-     *
-     * Typically, users are redirected here after authentication.
-     *
-     * @var string
-     */
-    public const HOME = '/home';
-
-    /**
      * Register any application services.
      */
     public function register(): void
@@ -45,15 +36,6 @@ class AppServiceProvider extends ServiceProvider
         //
 
         $this->bootRoute();
-
-        $this->bootRoute();
-    }
-
-    public function bootRoute(): void
-    {
-        RateLimiter::for('api', function (Request $request) {
-            return Limit::perMinute(60)->by($request->user()?->id ?: $request->ip());
-        });
     }
 
     public function bootRoute(): void
--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,7 +3,7 @@
 declare(strict_types=1);
 
 use App\Providers\AppServiceProvider;
-use Illuminate\Foundation\Application;
+use Pollora\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 
