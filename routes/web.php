e19ca463e4c4d188717649bfa4a5a3f7a4f54f55
Olivier Gorzalka <olivier@amphibee.fr>
2025-02-27 10:47:09 +0100

    Clean app

--- routes/web.php
+++ routes/web.php
@@ -16,8 +16,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', [HomeController::class, 'index'])
-    ->name('home');
+Route::get('/', function () {
+    return view('welcome');
+});
 
 /**
  * WordPress routes
